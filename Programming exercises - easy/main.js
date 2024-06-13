e9d9c1fc4df64c000ce220413690ce0fccb3731f
Vilém Rocha Leão <134015531+viliRocha@users.noreply.github.com>
2024-06-13 14:17:07 -0300

    Update main.js

--- Programming exercises - easy/main.js
+++ Programming exercises - easy/main.js
@@ -1,5 +1,5 @@
 //Verifies if number is odd or par
-function verificar(number) {
+function verify(number) {
 if(number == 0) {
     return("null \n \n");
 }
@@ -11,35 +11,6 @@ else{
 }
 }
 
-document.addEventListener("keypress", e=>{
-    if(e.which == 48){
-        console.log(verificar(0));
-    }
-    if(e.which == 49){
-        console.log(verificar(1));
-    }
-    if(e.which == 50){
-        console.log(verificar(2));
-    }
-    if(e.which == 51){
-        console.log(verificar(3));
-    }
-    if(e.which == 52){
-        console.log(verificar(4));
-    }
-    if(e.which == 53){
-        console.log(verificar(5));
-    }
-    if(e.which == 54){
-        console.log(verificar(6));
-    }
-    if(e.which == 55){
-        console.log(verificar(7));
-    }
-    if(e.which == 56){
-        console.log(verificar(8));
-    }
-    if(e.which == 57){
-        console.log(verificar(9));
-    }
-});
+let userInput = prompt("Type a number: ");
+
+console.log(verify(userInput));
