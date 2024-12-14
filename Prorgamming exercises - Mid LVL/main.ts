9adc528c608c4cd703f132305aba41354a7eb9a6
Vilém Rocha Leão <134015531+viliRocha@users.noreply.github.com>
2024-12-14 00:47:28 -0300

    Update main.ts

--- Prorgamming exercises - Mid LVL/main.ts
+++ Prorgamming exercises - Mid LVL/main.ts
@@ -1,21 +1,21 @@
 //Verifies if the string is a palindrome
-function epulavel (texto: string) {
+function epulavel (text: string) {
     //replace: removes all spaces
     //toLowerCase: turns all characters to lowercase
-    var str = texto.replace(/\s/g, "").toLowerCase();
+    let str = text.replace(/\s/g, "").toLowerCase();
 
     /*
     *split: transforms values into arrays
     *reverse: reverses values of a matrix
     *join: joins matrix indexes(opposite of matrices)
     */
-    var reversa = str.split("").reverse().join("");
+    let reversa = str.split("").reverse().join("");
 
     if(str == reversa) {
-        return("true");
+        return(true);
     }
     else {
-        return("false");
+        return(false);
     }
 }
 console.log(epulavel("A base do teto desaba"));
@@ -24,9 +24,9 @@ console.log(epulavel("A base do teto desaba"));
 function evogal(trecho: string) {
     //vowels: "a", "u", "e", "i", "o"
 
-    var nVogais = 0;
+    let nVogais = 0;
 
-    var string = trecho.toLowerCase().split("");
+    let string = trecho.toLowerCase().split("");
 
     let vogais = "aeiou";
 
@@ -43,36 +43,36 @@ console.log(evogal("Ola Aninha e Mariazinha"));
 
 //Verifies its factorial
 function e_Fatorial(number: number) {
-number = Math.round(number);
+    number = Math.round(number);
 
-if(number < 0) {
-    number = number * -1;
-}
+    if(number < 0) {
+        number *= -1;
+    }
 
-let val = 1;
+    let val = 1;
 
-if(number == 0) {
-    return(1);
-}
+    if(number == 0) {
+        return(1);
+    }
 
-for(let f = 1; f <= number; f++) {
-    //val = 1*1
-    val *= f;
-}
+    for(let f = 1; f <= number; f++) {
+        //val = 1*1
+        val *= f;
+    }
     return(val);
 }
 
 console.log(e_Fatorial(0));
 
 
-let matris: any = [];
+let matrix: number[] = [];
 
-let big_Num = matris[0];
+let big_Num = matrix[0];
 
 function o_maior_num() {
-    for(let l = 0; l < matris.length; l++) {
-        if(matris[l] > big_Num) {
-            big_Num = matris[l];
+    for(let l = 0; l < matrix.length; l++) {
+        if(matrix[l] > big_Num) {
+            big_Num = matrix[l];
         }
     }
 }
