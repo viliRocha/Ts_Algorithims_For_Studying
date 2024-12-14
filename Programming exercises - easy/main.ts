64f1186167aa70ece2d59704a187192860ff40e1
Vilém Rocha Leão <134015531+viliRocha@users.noreply.github.com>
2024-12-14 00:40:46 -0300

    Update main.ts

--- Programming exercises - easy/main.ts
+++ Programming exercises - easy/main.ts
@@ -1,14 +1,14 @@
 //Verifies if number is odd or par
 function verify(number: number): string|null {
-if(number == 0) {
-    return("null \n \n");
-}
-else if(number % 2 == 0) {
-    return("pair \n \n");
-}
-else{
-    return("odd \n \n");
-}
+    if(number == 0) {
+        return(null);
+    }
+    else if(number % 2 == 0) {
+        return("pair");
+    }
+    else{
+        return("odd");
+    }
 }
 
 let userInput = prompt("Type a number: ");
@@ -16,138 +16,136 @@ let userInput = prompt("Type a number: ");
 console.log(verify(Number(userInput)));
 
 //  calculates the average between numbers
-    let notas = [4.2, 7.9, 3.6, 0.9, 2.3];
+let notas = [4.2, 7.9, 3.6, 0.9, 2.3];
     
-    function media(grades: any) {
-        let soma = 0;
+function media(grades: number[]) {
+    let sum = 0;
     
-        for(let i = 0; i < grades.length; i++) {
-            if(grades[i] > 10 || grades[i] < 0) {
-                console.error(grades[i] + `Not a possible grade!!! \n \n`)
-            }
-            else {
-                soma += grades[i]
-            }
+    for(let i = 0; i < grades.length; i++) {
+        if(grades[i] > 10 || grades[i] < 0) {
+            console.error(grades[i] + `Not a possible grade!!! \n \n`)
         }
-    
-        let m = soma / grades.length;
-        return(Math.round(m));
-    }
-    
-    console.log(media(notas) + "\n \n");
-    
-    // Calculates if the value is prime or not
-    function epri(valor: number) {
-        if(valor == 1 && valor % 2 == 0) {
-            return("false \n \n");
-        }
-    
         else {
-        for(let c = 2; c < valor; c++) {
-    
-            if(valor % c == 0) {
-                return("false \n \n");  
-            }
+            sum += grades[i]
         }
-        }
-        return("true \n \n");
     }
     
-    console.log("Prime: " + epri(361));
+    let m = sum / grades.length;
+    return(Math.round(m));
+}
     
+console.log(media(notas));
     
-    let f = [[3, 4, 9], 
-            [12, 8, 7]];
-    
-    let v = [[1.5, 2, 4.5], 
-            [6, 4, 3.5]];
-            
-    let o = [[0, 0 ,0], 
-            [0, 0, 0]];
-    
-    function somaMatrix(m0: any, m1: any, m2: any) {
-    
-        for(let l = 0; l < f.length; l++) {
-    
-            for(let c = 0; c <= f.length; c++ ) {
-                m0[l][c] = m1[l][c] + m2[l][c];
-    
-            }
-        }
-    
-        return(m0);
+// Calculates if the value is prime or not
+function isPri(val: number) {
+    if(val == 1 && val % 2 == 0) {
+        return(false);
     }
     
-    console.log(somaMatrix(o, f, v));
+    else {
+        for(let c = 2; c < val; c++) {
     
-    //  verifies if it is an identity matrix
-    let ident = [[5, 0, 0],
-                [0, 5, 0], 
-                [0, 0, 5]];
+            if(val % c == 0) {
+                return(false);  
+            }
+        }
+    }
+    return(true);
+}
     
-    function e_ident(m0: any) {
-        for(let l = 0; l < m0.length; l++) {
+console.log("Prime: ", isPri(361));
+    
+    
+let f = [[3, 4, 9], 
+        [12, 8, 7]];
+    
+let v = [[1.5, 2, 4.5], 
+        [6, 4, 3.5]];
             
-            for(let c = 0; c < m0.length; c++ ) {
+let o = [[0, 0 ,0], 
+        [0, 0, 0]];
     
-                if(l == c) {
-                    if(m0[l][c] != 5) {
-                        return("false \n \n");
+function addMatrix(m0: number[][], m1: number[][], m2: number[][]) {
+    
+    for(let l = 0; l < f.length; l++) {
+    
+        for(let c = 0; c <= f.length; c++ ) {
+            m0[l][c] = m1[l][c] + m2[l][c];
+        }
+    }
+    
+    return(m0);
+}
+    
+console.log(addMatrix(o, f, v));
+    
+//  verifies if it is an identity matrix
+let ident = [[5, 0, 0],
+            [0, 5, 0], 
+            [0, 0, 5]];
+    
+function e_ident(m0: number[][]) {
+    for(let l = 0; l < m0.length; l++) {
+            
+        for(let c = 0; c < m0.length; c++ ) {
+    
+            if(l == c) {
+                if(m0[l][c] != 5) {
+                        return(false);
                     }
                 }
     
                 else if(l != c) {
                     if(m0[l][c] != 0) {
-                        return("false \n \n");
+                        return(false);
                     }
                 }
-            }
         }
-        return("true \n \n");
     }
+    return(true);
+}
     
-    console.log(e_ident(ident) + "\n \n");
+console.log(e_ident(ident));
     
-    // Algorithm to find the biggest number in the array
-    let values = [9, 11, 13];
+// Algorithm to find the biggest number in the array
+let values = [9, 11, 13];
     
-    function find_big_num(values: any) {
-        let bgNum = values[0];
+function find_big_num(values: number[]) {
+    let bgNum = values[0];
       
-        for (let h = 0; h < values.length; h++) {
-            //  11 is bigger than 9 so bgNum = 11 and finally 11 is smaller than 13 so...
-            if (values[h] > bgNum) {
-              bgNum = values[h];
-          }
+    for (let h = 0; h < values.length; h++) {
+        //  11 is bigger than 9 so bgNum = 11 and finally 11 is smaller than 13 so...
+        if (values[h] > bgNum) {
+            bgNum = values[h];
         }
+    }
       
-        return bgNum;
-    }
+    return bgNum;
+}
     
     
-    const bgNum = find_big_num(values);
-    console.log(bgNum + "\n \n");
+const bgNum = find_big_num(values);
+console.log(bgNum);
     
     
-    //Algorithm that counts till 100 and prints Buzz if the value is divisible by 5 and Fizz if divisible by 3
-    
-    function counter() {
-        for(let k = 0; k <= 100; k++) {
-            let val = "";
+//Algorithm that counts till 100 and prints Buzz if the value is divisible by 5 and Fizz if divisible by 3   
+function counter() {
+    for(let k = 0; k <= 100; k++) {
+        let val = "";
 
-            //    This way Fizz Buzz can be printed in the same line without also printing Fizz and Buzz another time
-            if(k % 3 == 0) {
-                val = val + "Fizz";
-            }
-            else if(k % 5 == 0) {
-                val = val + "Buzz";
-            }
-            else {
-                val = val + k;
-            }
-
-            console.log(val);
+        //    This way Fizz Buzz can be printed in the same line without also printing Fizz and Buzz another time
+        if(k % 3 == 0) {
+            val = val + "Fizz";
         }
+        else if(k % 5 == 0) {
+            val = val + "Buzz";
+        }
+        else {
+            val = val + k;
+        }
+
+        console.log(val);
     }
+}
     
-    counter();
+counter();
